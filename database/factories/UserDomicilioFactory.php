b6e23d403e0e734570205ec6d993c7ee07531d1d
garciaraulcu <garciaraulcu@gmail.com>
2022-02-02 23:32:52 -0600

    Agragando Examen Back

---
+++ database/factories/UserDomicilioFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserDomicilioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'domicilio' => $this->faker->address(),
+            'numero_exterior' => $this->faker->numberBetween(11,98),
+            'colonia' => $this->faker->streetAddress(),
+            'cp' => $this->faker->numberBetween(11269,90265),
+            'ciudad' => $this->faker->city(),
+        ];
+    }
+}
--- database/seeders/UserDomicilioSeeder.php
+++ database/seeders/UserDomicilioSeeder.php
@@ -4,15 +4,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserDomicilioSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-         \App\Models\User::factory(100)->create();
+        //
+        \App\Models\UserDomicilio::factory(100)->create();
     }
 }
